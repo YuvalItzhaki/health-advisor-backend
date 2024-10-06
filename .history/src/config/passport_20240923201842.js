cc7e7991e4f32210a1ea1b44238b656734124bc0
yuvalitzhaki <yyuval1971@gmail.com>
2024-10-06 16:58:31 +0300

    remove .env file

--- .history/src/config/passport_20240923201842.js
+++ .history/src/config/passport_20240923201842.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALL_BACK_URL,
+      callbackURL: 'http://localhost:5001/api/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
